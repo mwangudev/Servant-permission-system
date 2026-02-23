eb2972f0f7a402358061c9c272e114e6632d7ea4
mwangudev <mwanguad72@gmail.com>
2026-02-23 09:46:16 +0300

    f

--- resources/views/leaves/create.blade.php
+++ resources/views/leaves/create.blade.php
@@ -48,7 +48,7 @@
 
                         <div class="form-group">
                             <label for="destination">Destination</label>
-                            <input type="text" class="form-control" name="destination" id="destination" required>
+                            <input type="text" class="form-control" name="destination" id="destination" placeholder="Where will you be during your leave?" required>
                         </div>
 
                         {{-- Reason --}}
