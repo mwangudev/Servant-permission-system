0d5fad7a1c2262071845e64b67379e9813e5bf0d
Anijay4rly <anicethnswillah@gmail.com>
2026-02-23 10:30:42 +0300

    dashboard etc changes

---
+++ resources/views/admin/layouts/employee-cards.blade.php
@@ -0,0 +1,59 @@
+<div class="col-lg-3 col-md-6 col-12">
+    <div class="small-box bg-info">
+        <div class="inner">
+            <h3>{{ $submittedCount ?? 0 }}</h3>
+            <p>My Requests</p>
+        </div>
+        <div class="icon">
+            <i class="fas fa-list"></i>
+        </div>
+        <a href="{{ route('leaves.showmy') }}" class="small-box-footer">
+            View Info <i class="fas fa-arrow-circle-right"></i>
+        </a>
+    </div>
+</div>
+
+<div class="col-lg-3 col-md-6 col-12">
+    <div class="small-box bg-warning">
+        <div class="inner">
+            <h3>{{ $pendingCount ?? 0 }}</h3>
+            <p>Pending</p>
+        </div>
+        <div class="icon">
+            <i class="fas fa-clock"></i>
+        </div>
+        <a href="{{ route('leaves.showmy') }}" class="small-box-footer">
+            View Info <i class="fas fa-arrow-circle-right"></i>
+        </a>
+    </div>
+</div>
+
+<div class="col-lg-3 col-md-6 col-12">
+    <div class="small-box bg-success">
+        <div class="inner">
+            <h3>{{ $approvedCount ?? 0 }}</h3>
+            <p>Approved</p>
+        </div>
+        <div class="icon">
+            <i class="fas fa-check-circle"></i>
+        </div>
+        <a href="{{ route('leaves.showmy') }}" class="small-box-footer">
+            View Info <i class="fas fa-arrow-circle-right"></i>
+        </a>
+    </div>
+</div>
+
+<div class="col-lg-3 col-md-6 col-12">
+    <div class="small-box bg-danger">
+        <div class="inner">
+            <h3>{{ $rejectedCount ?? 0 }}</h3>
+            <p>Rejected</p>
+        </div>
+        <div class="icon">
+            <i class="fas fa-times-circle"></i>
+        </div>
+        <a href="{{ route('leaves.showmy') }}" class="small-box-footer">
+            View Info <i class="fas fa-arrow-circle-right"></i>
+        </a>
+    </div>
+</div>
